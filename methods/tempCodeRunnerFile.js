37c4ee14a21247bfaadc4c32e70222cbbc7d44b3
Abdo <abdopoetry@gmail.com>
2019-06-10 01:45:57 +0200

    Remove temporary unnecessary files

--- methods/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-import { saveRole, savedRoles } from '../common/savedRoles';
-// import Role from '../classes/Role';
-
-// const createRole = (name) => {
-//   const newRole = new Role('name');
-//   saveRole(newRole);
-// };
-
-// export default createRole;
